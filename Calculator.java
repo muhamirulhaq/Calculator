04ea15c29a9bb322bb25c4ab48aa563625bc1515
Muhammad Amirul Haq <70356768+muhamirulhaq@users.noreply.github.com>
2020-10-08 03:44:20 +0800

    Create Calculator.java
    The beginning source code

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        float firstNumber, secondNumber;
+        String operator;
+        firstNumber = 1;
+        operator = "+";
+        secondNumber = 2;
+
+        float addition, subtraction, multiplication, division;
+        addition = firstNumber + secondNumber;
+        subtraction = firstNumber - secondNumber;
+        multiplication = firstNumber * secondNumber;
+        division = firstNumber / secondNumber;
+
+        System.out.println("result: " + addition);
+
+    }
+}
